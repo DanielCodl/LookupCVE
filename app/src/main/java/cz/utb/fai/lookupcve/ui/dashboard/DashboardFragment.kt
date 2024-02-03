2d31fdcc4b1cf06b5f450cf69006cccc87b9f7e9
DCODL <dd@dd.com>
2024-02-03 17:07:47 +0100

    Updated DashboardFragment.kt and DashboardFragment.xml to implement logic and UI for Rest API search and displaying the results on screen (data is also stored in Room database). Data for widgets is stored into DashboardFragmentViewModel so it survives app lifecycle events (such as screen rotation, etc.)

--- app/src/main/java/cz/utb/fai/lookupcve/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/cz/utb/fai/lookupcve/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
 import cz.utb.fai.lookupcve.databinding.FragmentDashboardBinding
+import retrofit2.Response
+import java.lang.NullPointerException
+import androidx.lifecycle.Observer
+import cz.utb.fai.lookupcve.api.ApiServices
+import cz.utb.fai.lookupcve.api.RetrofitInstance
+import cz.utb.fai.lookupcve.database.CveDTO
+import cz.utb.fai.lookupcve.database.CveViewModel
+import cz.utb.fai.lookupcve.model.CveJson
+
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var mCveViewModel: CveViewModel
+    private lateinit var dashboardViewModel:DashboardViewModel
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,16 +39,114 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
+        //val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        mCveViewModel =ViewModelProvider(this).get(CveViewModel::class.java)
+
+
+        // Observing Widgets on Fragment page using values from ViewModel and MutableLiveData
+        val tvl_id_txt: TextView = binding.idTxt
+        dashboardViewModel.id_txt.observe(viewLifecycleOwner) {
+            tvl_id_txt.text = it
         }
+        val tvl_baseScore_txt: TextView = binding.baseScoreTxt
+        dashboardViewModel.baseScore_txt.observe(viewLifecycleOwner) {
+            tvl_baseScore_txt.text = it
+        }
+        val tvl_baseSeverity_txt: TextView = binding.baseSeverityTxt
+        dashboardViewModel.baseSeverity_txt.observe(viewLifecycleOwner) {
+            tvl_baseSeverity_txt.text = it
+        }
+        val tvl_vectorString_txt: TextView = binding.vectorStringTxt
+        dashboardViewModel.vectorString_txt.observe(viewLifecycleOwner) {
+            tvl_vectorString_txt.text = it
+        }
+        val tvl_published_txt: TextView = binding.publishedTxt
+        dashboardViewModel.published_txt.observe(viewLifecycleOwner) {
+            tvl_published_txt.text = it
+        }
+        val tvl_description_txt: TextView = binding.descriptionTxt
+        dashboardViewModel.description_txt.observe(viewLifecycleOwner) {
+            tvl_description_txt.text = it
+        }
+
+        val pb_progressBar: ProgressBar = binding.progressBar
+        dashboardViewModel.ProgressBarVisible.observe(viewLifecycleOwner) {
+            pb_progressBar.visibility = it
+        }
+
+        // Submit CVE number to REST API call - to fetch the data from Internet
+        val bt_submit_cve_button = binding.btSubmitCve
+
+        bt_submit_cve_button.setOnClickListener(){
+            if (binding.evCveNum.text.isEmpty()){
+                Toast.makeText(requireContext(), "CVE number cannot be empty!", Toast.LENGTH_SHORT).show()
+            } else{
+                // Beginning to fetch the data from Internet - Set the Progress Bar indicator ON
+                dashboardViewModel.ProgressBarVisible.value=View.VISIBLE
+                val retrofitService =
+                    RetrofitInstance.getRetrofitInstance().create(ApiServices::class.java)
+
+                // Retrofit call - REST API
+                val responseLiveData: LiveData<Response<CveJson>> =
+                    liveData {
+                        val response = retrofitService.getCveJson(binding.evCveNum.text.toString())
+                        emit(response)
+                    }
+
+                // Response received, now we need to process it
+                responseLiveData.observe(viewLifecycleOwner, Observer {
+                    // Handle API call errors - output is NULL (eg. when wrong CVE or mismatch input is provided)
+                    if (it.body() != null) {
+
+                        // Json response to Data Object mapping
+                        val cveid: String = it.body()?.vulnerabilities!![0].cve.id
+                        val published: String = it.body()?.vulnerabilities!![0].cve.published
+                        val description: String = it.body()?.vulnerabilities!![0].cve.descriptions[0].value
+                        dashboardViewModel.id_txt.value=cveid
+                        dashboardViewModel.published_txt.value=published
+                        dashboardViewModel.description_txt.value=description
+
+                        // Additional check for NulPointer needed in case of missing cvssMetricV31 data (for old CVEs)
+                        var baseScore: Double = 0.0
+                        var baseSeverity: String = "N/A"
+                        var vectorString: String = "N/A"
+
+                        try {
+                            baseScore = it.body()?.vulnerabilities!![0].cve.metrics.cvssMetricV31[0].cvssData.baseScore
+                            baseSeverity = it.body()?.vulnerabilities!![0].cve.metrics.cvssMetricV31[0].cvssData.baseSeverity
+                            vectorString = it.body()?.vulnerabilities!![0].cve.metrics.cvssMetricV31[0].cvssData.vectorString
+                        } catch (e: NullPointerException) {
+                            // Fetch older cvssMetricV2 data (for older CVEs) if it exists
+                        }
+
+                        dashboardViewModel.vectorString_txt.value=vectorString
+                        dashboardViewModel.baseSeverity_txt.value=baseSeverity
+                        dashboardViewModel.baseScore_txt.value=baseScore.toString()
+
+                        val record = CveDTO(0, cveid, published, description, baseScore, baseSeverity, vectorString)
+
+                        // Store data to Room database
+                        mCveViewModel.addCve(record)
+
+                        Toast.makeText(requireContext(), "Success!", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        Toast.makeText(requireContext(), "Error fetching data from REST API - CVE \""+binding.evCveNum.text.toString()+"\" does NOT exist!", Toast.LENGTH_LONG).show()
+                    }
+                    binding.evCveNum.text.clear()
+                    // Finished to fetch the data from Internet - Set the Progress Bar indicator back OFF
+                    dashboardViewModel.ProgressBarVisible.value=View.INVISIBLE
+                })
+
+            }
+        }
+
         return root
     }
 
--- app/src/main/java/cz/utb/fai/lookupcve/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/cz/utb/fai/lookupcve/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,43 @@
 package cz.utb.fai.lookupcve.ui.dashboard
 
-import androidx.lifecycle.LiveData
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _id_txt = MutableLiveData<String>().apply {
+        value = ""// "xCVE-2019-1010218"
     }
-    val text: LiveData<String> = _text
+    private val _baseScore_txt = MutableLiveData<String>().apply {
+        value = ""// "x7.5"
+    }
+    private val _baseSeverity_txt = MutableLiveData<String>().apply {
+        value = ""// "xHIGH"
+    }
+    private val _vectorString_txt = MutableLiveData<String>().apply {
+        value = ""// "xCVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H"
+    }
+    private val _published_txt = MutableLiveData<String>().apply {
+        value = ""// "x2019-07-22T18:15:10.917"
+    }
+    private val _description_txt = MutableLiveData<String>().apply {
+        value = ""// "xCherokee Webserver Latest Cherokee Web server Upto Version 1.2.103 (Current stable) is affected by: Buffer Overflow - CWE-120. The impact is: Crash. The component is: Main cherokee command. The attack vector is: Overwrite argv[0] to an insane length with execl. The fixed version is: There's no fix yet."
+    }
+
+    private val _ProgressBarVisible = MutableLiveData<Int>().apply {
+        value = View.INVISIBLE
+    }
+
+    val id_txt: MutableLiveData<String> = _id_txt
+    val baseScore_txt: MutableLiveData<String> = _baseScore_txt
+    val baseSeverity_txt: MutableLiveData<String> = _baseSeverity_txt
+    val vectorString_txt: MutableLiveData<String> = _vectorString_txt
+    val published_txt: MutableLiveData<String> = _published_txt
+    val description_txt: MutableLiveData<String> = _description_txt
+
+    val ProgressBarVisible: MutableLiveData<Int> =_ProgressBarVisible
+
 }
+
+
