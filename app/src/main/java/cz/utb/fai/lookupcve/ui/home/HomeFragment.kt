b49f940a2fd1e11014f567575e655b51e3b49231
DCODL <dd@dd.com>
2024-02-07 08:53:43 +0100

    Updated Home fragment with Welcome screen

--- app/src/main/java/cz/utb/fai/lookupcve/ui/home/HomeFragment.kt
+++ app/src/main/java/cz/utb/fai/lookupcve/ui/home/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
@@ -70,10 +69,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
